5b44daef9f571c7c64deed39b784c295c8db1dff
hpatel00 <hpatel2356@gmail.com>
2022-04-14 14:49:47 -0400

    Updated Jenkinsfile for shared roboshop library

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -3,7 +3,7 @@ def lintChecks()
     sh '''
        # We commented this out bc there are errors that the developers would need to fix
        #~/node_modules/jslint/bin/jslint.js server.js
-       echo Lint Check
+       echo Lint Check for ${COMPONENT}
     '''
 }
 
