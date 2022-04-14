ce7a7d2af2d56d078af4d09c294ab118aa0e1276
hpatel00 <hpatel2356@gmail.com>
2022-04-14 19:44:37 -0400

    updated golang shared libraries

---
+++ vars/golang.groovy
@@ -0,0 +1,40 @@
+def lintChecks()
+{
+    sh '''
+       # We commented this out bc there are errors that the developers would need to fix
+       #~/node_modules/jslint/bin/jslint.js server.js
+       echo Lint Check for ${COMPONENT}
+    '''
+}
+
+def call() {
+    pipeline {
+        agent any
+
+        environment {
+            SONAR = credentials('SONAR')
+        }
+
+        stages{
+
+            // For each commit
+            stage('Lint Check') {
+                steps{
+                    script{
+                        lintChecks()
+                    }
+                }
+            }
+
+            stage('SonarCheck') {
+                steps{
+                    script{
+                        env.ARGS="-Dsonar.sources=."
+                        common.sonarCheck()
+                    }
+                }
+            }
+        } // end of stages
+    }
+
+}
