203fab9f921d9d3edbf250b8162c1df9b9209087
hpatel00 <hpatel2356@gmail.com>
2022-04-14 15:18:57 -0400

    Updated Jenkinsfile for python component

--- vars/python.groovy
+++ vars/python.groovy
@@ -3,7 +3,7 @@ def lintChecks()
     sh '''
        # We commented this out bc there are errors that the developers would need to fix
        #~/node_modules/jslint/bin/jslint.js server.js
-       #mvn checkstyle:check
+       pylint *.py
        echo Lint Check for ${COMPONENT}
     '''
 }
