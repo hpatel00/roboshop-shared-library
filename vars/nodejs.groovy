e15cde7935a2671a1f06ed6fff9358060a3b7a8e
hpatel00 <hpatel2356@gmail.com>
2022-04-14 18:12:01 -0400

    Added sonarqube checks for nodejs shared library

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -9,13 +9,17 @@ def lintChecks()
 
 def sonarCheck() {
     sh '''
-       sonar-scanner -Dsonar.host.url=http://172.31.9.186:9000 -Dsonar.sources=. -Dsonar.projectKey=${COMPONENT}
+       sonar-scanner -Dsonar.host.url=http://172.31.9.186:9000 -Dsonar.sources=. -Dsonar.projectKey=${COMPONENT} -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW}
     '''
 }
 
 def call() {
     pipeline {
         agent any
+
+        environment {
+            SONAR = credentials('SONAR')
+        }
 
         stages{
 
